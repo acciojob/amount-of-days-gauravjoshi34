3197108ae5753a3a2906543a13a2d48706c8935b
gaurav234 <91665015+gauravjoshi34@users.noreply.github.com>
2025-07-28 17:51:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if(year%4==0){
+		if(year%100==0){
+			if(year%400==0){
+				console.log("365");
+			}
+		}else{
+			console.log("365");
+		}
+	}else{
+		console.log("366");
+	}
+}
+
+let year = Number(prompt("Enter the year:"));
+daysOfAYear(year);
